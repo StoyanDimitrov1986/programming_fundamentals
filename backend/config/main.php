df531b5c2f3ea154fda9306022f345b8cc54fecc
StoyanDimitrov1986 <energon_themaelstrom@abv.bg>
2022-05-24 12:35:45 +0300

    Changed app name

--- backend/config/main.php
+++ backend/config/main.php
@@ -8,6 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-backend',
+    'name'=>'(Teacher) Programming Fundamentals',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
