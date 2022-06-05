31898492583609a1dd9b152e75f52534045c1205
StoyanDimitrov1986 <energon_themaelstrom@abv.bg>
2022-06-05 10:46:14 +0300

    Changed tab title

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -2,7 +2,7 @@
 
 /** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+$this->title = '(Teacher) Programming fundamentals';
 ?>
 <div class="site-index">
 
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,7 +2,7 @@
 
 /** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+$this->title = 'Programming fundamentals';
 ?>
 <div class="site-index">
 
