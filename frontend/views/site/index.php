0193ba2fec306c96f34f6eb37da067d6fe299543
StoyanDimitrov1986 <energon_themaelstrom@abv.bg>
2022-06-26 11:37:22 +0300

    More home page centering

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,16 +1,16 @@
 <?php
+
 use yii\helpers\Url;
 
 /** @var yii\web\View $this */
 
 $this->title = 'Programming fundamentals';
 ?>
-<div class="site-index">
+<div class="site-index" style="margin-top: 10%">
 
     <div class="jumbotron text-center bg-transparent">
         <h1 class="display-4">Welcome to<br>Programming Fundamentals</h1>
-
-        <br>
+        <p class="lead">The place where you could test your programming skills.</p>
         <?php if (Yii::$app->user->isGuest) {
             echo "<p><a class='btn btn-lg btn-success' href='" . Url::to('site/login') . "'>Login</a></p>";
         } else {
